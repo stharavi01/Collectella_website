0054d0cec7fd0b0a1e0ac7966860a0d21078028d
sanayashe <sanayashe@gmail.com>
2023-08-26 15:19:34 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Get references to the banner and button elements
+const banner = document.getElementById('banner');
+const changeButton = document.getElementById('changeButton');
+
+// Define an array of background image URLs that you want to cycle through
+const backgroundImageURLs = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+];
+
+// Initialize an index to keep track of the current background image
+let currentIndex = 0;
+
+// Function to change the background image
+function changeBackground() {
+    // Increment the index to the next background image URL
+    currentIndex = (currentIndex + 1) % backgroundImageURLs.length;
+
+    // Set the background image URL
+    banner.style.backgroundImage = `url(${backgroundImageURLs[currentIndex]})`;
+}
+
+// Add a click event listener to the button
+changeButton.addEventListener('click', changeBackground);
+
+// Initial background image
+changeBackground();
